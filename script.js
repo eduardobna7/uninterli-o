f5ce5447847fe180f817e85d4e63a24cd6244f59
eduardobna7 <102392454+eduardobna7@users.noreply.github.com>
2025-07-03 22:44:35 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+let botao=document.querySelector("#botao");
+botao.style.background="bisque";
+botao.addEventListener("click",e =>{botao.innerHTML="Bem-vindo(a) ao exercício básico de front-end!"});
